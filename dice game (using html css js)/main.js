3fe5d19a21d9f39be25015e2acb488aaf80728d7
Deekshith B <167673489+deekshithikify@users.noreply.github.com>
2025-04-26 11:46:17 +0530

    Update main.js

--- dice game (using html css js)/main.js
+++ dice game (using html css js)/main.js
@@ -1,35 +1,17 @@
-const randNumber1 =Math.floor(Math.random()*6) + 1;
-console.log(randNumber1);
+const randNumber1 = Math.floor(Math.random() * 6) + 1;
+const randNumber2 = Math.floor(Math.random() * 6) + 1;
 
-const diceNumber = `dice${randNumber1}.png`;
-console.log(diceNumber);
-
-const diceImage = `images/${diceNumber}`;
-console.log(diceImage);
-
-const image = document.querySelector(".img1");
-
-image.setAttribute("src", diceImage);
-
-
-const randNumber2 =Math.floor(Math.random()*6) + 1;
-console.log(randNumber1);
-
-const diceNumber2 = `dice${randNumber2}.png`;
-console.log(diceNumber2);
-
-const diceImage2 = `images/${diceNumber2}`;
-console.log(diceImage2);
-
-const image2 = document.querySelector(".img2");
-
-image2.setAttribute("src", diceImage2);
+document.querySelector(".img1").setAttribute("src", `images/dice${randNumber1}.png`);
+document.querySelector(".img2").setAttribute("src", `images/dice${randNumber2}.png`);
 
 if (randNumber1 > randNumber2) {
     document.querySelector("h1").innerHTML = "Player 1 Won!!";
-}else if (randNumber2 > randNumber1) {
+} else if (randNumber2 > randNumber1) {
     document.querySelector("h1").innerHTML = "Player 2 Won!!";
-}else {
+} else {
     document.querySelector("h1").innerHTML = "Tie";
 }
 
+function restartGame() {
+    location.reload();
+}
